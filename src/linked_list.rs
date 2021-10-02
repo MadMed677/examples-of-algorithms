f1d4ab0c0cf2dc49caa8aaee6656083f6be0a083
Artem Anashev <artem.a@miro.com>
2021-10-02 17:59:00 +0500

    Implemented `insert_sorted` method

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,9 +1,10 @@
 use std::cmp::PartialOrd;
+use std::fmt::Display;
 
 #[derive(Debug)]
-pub struct LinkedList<T: PartialEq>(Option<(T, Box<LinkedList<T>>)>);
+pub struct LinkedList<T: PartialOrd + Display>(Option<(T, Box<LinkedList<T>>)>);
 
-impl<T: PartialEq> LinkedList<T> {
+impl<T: PartialOrd + Display> LinkedList<T> {
     pub fn new() -> Self {
         LinkedList(None)
     }
@@ -19,6 +20,28 @@ impl<T: PartialEq> LinkedList<T> {
             None => self.push_front(data),
         }
     }
+
+    pub fn insert_sorted(&mut self, data: T) {
+        match self.0 {
+            Some((ref value, ref mut child)) => {
+                if &data >= value {
+                    println!(
+                        "Found the point. The current value is: {} and the next value is: {}",
+                        &data, value
+                    );
+                    child.insert_sorted(data);
+                } else {
+                    println!(
+                        "Didn't find the point. The current value is: {} and the next value is: {}",
+                        &data, value
+                    );
+
+                    self.push_front(data);
+                }
+            }
+            None => self.push_back(data),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -26,13 +49,21 @@ mod linked_list {
     use crate::linked_list::LinkedList;
 
     #[test]
-    fn it_works() {
+    fn should_create_linked_list() {
         let mut linked_list = LinkedList::new();
         linked_list.push_front(2);
         linked_list.push_back(1);
+    }
+
+    #[test]
+    fn should_insert_via_insert_sorted() {
+        let mut linked_list = LinkedList::new();
+        linked_list.push_front(2);
+        linked_list.push_back(4);
+        linked_list.push_front(1);
+        linked_list.push_back(5);
+        linked_list.insert_sorted(3);
 
         println!("{:?}", linked_list);
-
-        assert_eq!(2 + 2, 4);
     }
 }
